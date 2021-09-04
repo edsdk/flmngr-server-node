28e7a6a184229448da53f592641d7817bd79281f
dmitriy-komarov <dmitriy-komarov@3627127.no-reply.drupal.org>
2021-09-04 13:59:55 +0300

    Debug configuration improvement

--- src/debug-server.ts
+++ src/debug-server.ts
@@ -12,26 +12,32 @@ const app = express();
 // Load environment variables from .env file, where API keys and passwords are configured
 dotenv.config({path: '.env'});
 
+if (!process.env.FLMNGR_PORT || !process.env.FLMNGR_URL_PATH || !process.env.FLMNGR_DIR) {
+	console.log("See .env.sample, follow instructions and re-run this debug app.");
+	process.exit(-1);
+}
+
 app.use(errorHandler());
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.FLMNGR_PORT);
 app.use(morgan('combined'));
 app.use(compression());
 
-app.use('/api', (_: Request, res: Response, next: NextFunction): void => {
+app.use('/api', (_: Request, res: Response, next: NextFunction): void => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
 
-bindFlmngr({app, url: '/api', dir: process.env.UPLOAD_DIR_PATH!});
+bindFlmngr({app, url: process.env.FLMNGR_URL_PATH, dir: process.env.FLMNGR_DIR});
 
-app.use(express.static(process.env.UPLOAD_DIR_PATH!));
+//app.use(express.static(process.env.FLMNGR_DIR));
 
 app.listen(app.get('port'), () => {
 	console.log(
-		'  App is running at http://localhost:%d in %s mode',
-		app.get('port'),
-		app.get('env'),
+		'  App is running at http://localhost:%d%s in %s mode',
+		process.env.FLMNGR_PORT,
+		process.env.FLMNGR_URL_PATH,
+		process.env.FLMNGR_DIR,
 	);
 	console.log('  Press CTRL-C to stop\n');
 });
